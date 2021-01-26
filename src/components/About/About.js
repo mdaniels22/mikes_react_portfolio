8c62679a9f35c4765a202ca72e5fd78437166bd3
mdaniels22 <mikedaniels22@gmail.com>
2021-01-26 11:50:45 -0500

    worked on Skills section

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,49 +1,76 @@
-import React from "react";
+import React, { useState } from "react";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Image from "react-bootstrap/Image";
 import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
 import "./About.css";
 import aboutPic from "../../media/undraw_dev_productivity_umsq.svg";
+import Resume from "../../media/Michael_Daniels.pdf";
 
-const about = (props) => (
-  <Container id="About">
-    <h1>About Me</h1>
-    <div className="About">
-      <div>
-        <Image src={aboutPic} fluid />
-      </div>
-      <div className="AboutText">
-        <h2>Hi, I'm Mike and Develop for the Web</h2>
-        <p>
-          I've been in the Tech industry for over 10 years. I began in tech
-          support and worked up to Systems Technician. Support roles didn't
-          fulfill my need to create. I figured I can use my knowledge of
-          programming and my creativity to create applications and websites.
-          After I designed and devloped my first working application I was
-          hooked. I've been developing and designing web applications and sites
-          since then. The sites I've created have significantly increased
-          attention and sales for clients. I constantly code to improve my
-          skills and to stay current with the latest technologies. My current
-          stack includes HTML5, CSS3, JavaScript, React.js, and Node.js with
-          Express.js. Bootstrap, npm, Git, and GIMP are some of the tools I use
-          and I've had exprerience coding with C#, C++, and Java.
-        </p>
-        {/* <Col>Name: {props.name}</Col>
+function About(props) {
+  const [show, setShow] = useState(false);
+
+  const handleShow = () => {
+    setShow(true);
+  };
+  const handleClose = () => {
+    setShow(false);
+  };
+
+  return (
+    <Container id="About">
+      <h1>About Me</h1>
+      <div className="About">
+        <div>
+          <Image className="AboutPic" src={aboutPic} />
+        </div>
+        <div className="AboutText">
+          <h2>Hi, I'm Mike and Develop for the Web</h2>
+          <p>
+            I've been in the Tech industry for over 10 years. I began in tech
+            support and worked up to Systems Technician. Support roles didn't
+            fulfill my need to create. I figured I can use my knowledge of
+            programming and my creativity to create applications and websites.
+            After I designed and devloped my first working application I was
+            hooked. I've been developing and designing web applications and
+            sites since then. The sites I've created have significantly
+            increased attention and sales for clients. I constantly code to
+            improve my skills and to stay current with the latest technologies.
+            My current stack includes HTML5, CSS3, JavaScript, React.js, and
+            Node.js with Express.js. Bootstrap, npm, Git, and GIMP are some of
+            the tools I use and I've had exprerience coding with C#, C++, and
+            Java.
+          </p>
+          {/* <Col>Name: {props.name}</Col>
         <Col>From: {props.location}</Col>
         <Col>Email: {props.email}</Col> */}
-        <Row>
-          <Col>
-            <Button>Resume</Button>
-          </Col>
-          <Col>
-            <Button href="#Projects">Projects</Button>
-          </Col>
-        </Row>
+          <Row className="AboutButton">
+            <Col>
+              <Button onClick={handleShow}>Resume</Button>
+              <Modal size="lg" show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                  <Modal.Title>Resume</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                  <embed
+                    src={Resume}
+                    type="application/pdf"
+                    width="100%"
+                    height="650px"
+                  />
+                </Modal.Body>
+              </Modal>
+            </Col>
+            <Col>
+              <Button href="#Projects">Projects</Button>
+            </Col>
+          </Row>
+        </div>
       </div>
-    </div>
-  </Container>
-);
+    </Container>
+  );
+}
 
-export default about;
+export default About;
--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -10,38 +10,41 @@ import react_logo from "../../media/react-white-logo.com.svg";
 import node_express_logo from "../../media/node_express_logo.png";
 
 const Skills = () => (
-  <div id="Skills" className="Skills">
+  <div id="Skills">
     <h1>Skills</h1>
-    <p>
-      I have experience with several coding tools and language, such as HTML,
-      CSS, Javascript, C#, Java, React, NodeJs, VB.NET, and Bootstrap. My
-      current stack includes HTML5, CSS3, JavaSript, React.js and Express.js.
-    </p>
-    <Container fluid>
-      <Row>
-        <Col>
-          <Skill
-            image={html_css_js_logo}
-            title="FrontEnd Development"
-            description="I use HTML, CSS, and JavaScript to make engaging and interactive sites. A few examples of my sites can be seen in my projects section"
-          />
-        </Col>
-        <Col>
-          <Skill
-            image={react_logo}
-            title="FrontEnd Design"
-            description="I love to use React.js when I am creating and designing reusable components. An example of this is the Sandwich builder, along with other projects."
-          />
-        </Col>
-        <Col>
-          <Skill
-            image={node_express_logo}
-            title="BackEnd Development"
-            description="I use Node.js in conjuction with Express.js for server-side rendering. I found Express.js to be easy to learn and implement."
-          />
-        </Col>
-      </Row>
-    </Container>
+    <div className="Skills">
+      {" "}
+      <p className="SkillsText">
+        I have experience with several coding tools and language, such as HTML,
+        CSS, Javascript, C#, Java, React, NodeJs, VB.NET, and Bootstrap. My
+        current stack includes HTML5, CSS3, JavaSript, React.js and Express.js.
+      </p>
+      <Container fluid>
+        <Row className="SkillsCards">
+          <Col>
+            <Skill
+              image={html_css_js_logo}
+              title="FrontEnd Development"
+              description="I use HTML, CSS, and JavaScript to make engaging and interactive sites. A few examples of my sites can be seen in my projects section"
+            />
+          </Col>
+          <Col>
+            <Skill
+              image={react_logo}
+              title="FrontEnd Design"
+              description="I love to use React.js when I am creating and designing reusable components. An example of this is the Sandwich builder, along with other projects."
+            />
+          </Col>
+          <Col>
+            <Skill
+              image={node_express_logo}
+              title="BackEnd Development"
+              description="I use Node.js in conjuction with Express.js for server-side rendering. I found Express.js to be easy to learn and implement."
+            />
+          </Col>
+        </Row>
+      </Container>
+    </div>
   </div>
 );
 
